fd7c9f744e0f5f7d3bda35f2055b158556361ca3
Рулан <temnihan@mail.ru>
2022-08-08 18:59:52 +0300

    forth

---
+++ 1Day/task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number: ");
+int count = int.Parse(Console.ReadLine());
+int i = 1;
+while (i<=count)
+{
+    if (i/2*2 == i)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
