e44e52b6950c6ac9d8070c32eddf3a7bdc866b0e
Рулан <temnihan@mail.ru>
2022-08-08 18:33:01 +0300

    first task

---
+++ 1Day/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Enter number b: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("max = : ");
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
