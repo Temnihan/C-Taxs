a04bab49130629be9ecc0fa3ec1abb182a7a9ec1
Рулан <temnihan@mail.ru>
2022-08-16 21:29:00 +0300

    19 task

---
+++ 3Day/19task/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Enter numbers of 5 number: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 10 == num /10000 &&  num %100/10 == num/1000%10){
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("NO");
+}
