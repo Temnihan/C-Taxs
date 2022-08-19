a9456f84889acb446201769cdcef1056a55639a5
Рулан <temnihan@mail.ru>
2022-08-19 21:17:52 +0300

    27 task

---
+++ 4Day/27task/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите функцию и запустите ее, которая принимает на вход число 
+//  и выдаёт сумму цифр в числе.
+int sumAllNumbers(int number)
+{
+
+    int sum = number % 10;
+    while (number > 9)
+    {
+        number = number / 10;
+        sum += number % 10;
+    }
+    return sum;
+}
+Console.WriteLine("Enter Nuber: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine(sumAllNumbers(N));
