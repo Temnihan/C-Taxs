57108f8c10e9068440b7a647098673ea818f3605
Рулан <temnihan@mail.ru>
2022-08-19 20:56:49 +0300

    25 task

---
+++ 4Day/25task/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B)
+//   и возводит число A в натуральную степень B.
+int pw(int A, int B){
+    int rezult = 1;
+    for(int i = 1; i<=B; i++){
+        rezult *=A;
+    }
+    return rezult;
+}
+Console.WriteLine("Enter A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(pw(a,b));
