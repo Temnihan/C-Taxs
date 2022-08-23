8c27845cb3b5787095c3cb1a9cb357d6705960e3
Рулан <temnihan@mail.ru>
2022-08-23 10:47:34 +0300

    38 task

---
+++ 5Day/38task/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу
+//  между максимальным и минимальным элементов массива
+int[] arr(int size, int st, int end)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(st, end + 1);
+    }
+    return res;
+}
+Console.WriteLine(" Enter size massiv");
+int s = int.Parse(Console.ReadLine());
+int[] array = arr(s, 0, 10);
+Console.WriteLine(string.Join(" ", array));
+int difer(int[] mas)
+{
+    int mx = mas[0];
+    int mn = mas[0];
+    foreach (int el in mas){
+        if (el > mx){
+            mx = el;
+        }
+        else if (el <mn){
+            mn = el;
+        }
+        
+    }
+return mx -mn;
+}
+Console.WriteLine(difer(array));
