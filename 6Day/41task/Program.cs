71ba6e32e723ce407147f564f623cc6762c7f68c
Рулан <temnihan@mail.ru>
2022-08-25 20:40:19 +0300

    41 task

---
+++ 6Day/41task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.WriteLine("Enter size Array");
+int num = int.Parse(Console.ReadLine());
+int  ElementsMoreZero( int m){
+    int result = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if ( int.Parse(Console.ReadLine())>0) result++;
+    }
+    return result;
+}
+Console.WriteLine($" elem more 0 is: {ElementsMoreZero(num)}");
