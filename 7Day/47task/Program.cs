a79f87368abd543b07f04db54014d3d474fc5f7a
Рулан <temnihan@mail.ru>
2022-09-07 17:41:33 +0300

    47 task dobule array

---
+++ 7Day/47task/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+Console.Write("m = :");
+int m =  int.Parse(Console.ReadLine());
+Console.Write("n = :");
+int n = int.Parse(Console.ReadLine());
+void PrintArray( double[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($" {array[i,j]}");
+               
+            }        
+        Console.WriteLine();
+        } 
+}
+
+double [,] GetArray(int m, int n){
+    double [,] result = new double [m,n];
+    for (int i = 0; i < m; i++)
+        { 
+        for (int j = 0; j < n; j++)
+            {
+                result[i,j] = Math.Round(new  Random().NextDouble(), 2);
+                // result[i,j] = new  Random().NextDouble();
+            
+            }
+        }
+        return result;
+    }
+double [,] myArray = GetArray(m,n);
+PrintArray(myArray);
