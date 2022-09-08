a133d909a6a81d3f167b2759fda0542d226c1a8b
Рулан <temnihan@mail.ru>
2022-09-08 14:19:42 +0300

    52 task

---
+++ 7Day/52Task/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+//  1    ----- create Array  -----------------
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+
+        }
+    }
+    return result;
+}
+int m = new Random().Next(1, 9);
+int n = new Random().Next(3, 10);
+int[,] myArray = GetArray(m, n, 0, 100);
+// 2.  ---- Show Array  -----------
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+
+}
+PrintArray(myArray);
+
+void Arifmetich(int[,] array)
+{
+    int[] newArray = new int[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[j]+=array[i,j];
+        }
+    }
+ 
+    Console.WriteLine(string.Join(" ",newArray));
+
+}
+Console.WriteLine();
+Arifmetich(myArray);
