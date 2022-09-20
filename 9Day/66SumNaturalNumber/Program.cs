00d75f98f265f5f9b4933490c30e691ef6507851
Рулан <temnihan@mail.ru>
2022-09-20 17:46:47 +0300

    66 Sum Natural Number

---
+++ 9Day/66SumNaturalNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N
+Console.Write(" Enter start: ");
+int start = int.Parse(Console.ReadLine());
+Console.Write(" Enter  end:  ");
+int end = int.Parse(Console.ReadLine());
+//  recursiv
+int allSum(int A, int B)
+{
+    if (B == A) return A;
+
+    return B + allSum(A,B-1);
+}
+Console.WriteLine(allSum(start,end));
+// better variant
+int allSum2(int A, int B)
+{
+    return (A+B)*(B-A +1)/2;
+}
+Console.WriteLine(allSum2(start,end));
